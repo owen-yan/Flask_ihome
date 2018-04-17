c3c2307f7a21f8204cb8b1569c6cc0e950dde3e4
xwp <xwp_fullstack@163.com>
2018-04-17 20:06:23 +0800

    配置redis数据库和mysql数据库

--- manager.py
+++ manager.py
@@ -1,19 +1,39 @@
 # -*- coding:utf-8 -*-
 # 程序入口
-
+import redis
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+
 
 class Config(object):
     """配置工程信息"""
     DEBUG = True
 
+    # 数据库的配置信息,真实的开发不会写127 这个ip地址会写真实的ip地址
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis数据库配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+
+app = Flask(__name__)
+
+# 加载配置参数
 app.config.from_object(Config)
+
+# 链接mysql数据库
+db = SQLAlchemy(app)
+
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 
 @app.route('/')
 def index():
+
+    redis_store.set('name', 'owen')
     return 'index'
 
 
