2bf8dc39919ef1ea50d793f585169a03adb52015
xwp <xwp_fullstack@163.com>
2018-04-17 19:21:28 +0800

    初始化版本

---
+++ manager.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+# 程序入口
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
