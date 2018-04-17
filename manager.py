aaed23a8d6f9375a217ac4036b16d0a429458170
xwp <xwp_fullstack@163.com>
2018-04-17 19:41:22 +0800

    配置工程信息

--- manager.py
+++ manager.py
@@ -5,6 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Config(object):
+    """配置工程信息"""
+    DEBUG = True
+
+app.config.from_object(Config)
+
 
 @app.route('/')
 def index():
@@ -12,4 +18,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run( )
